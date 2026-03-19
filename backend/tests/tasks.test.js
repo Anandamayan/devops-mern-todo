088800bfcf9199833579b6b95ae60a5cb5b16336
anandamayan <msanandamayan@gmail.com>
2026-03-19 23:03:11 +0530

    Initial Commit

---
+++ backend/tests/tasks.test.js
@@ -0,0 +1,22 @@
+const request = require('supertest');
+const {server,app} = require('../index');
+const mongoose = require('mongoose');
+
+describe('GET api/tasks',() => {
+    it('It Should return 200 ok',async()=>{
+        const res = await request(app).get('/api/tasks')
+        expect(res.statusCode).toBe(200);
+    })
+    it('It Should return an object and tasks property',async()=>{
+        const res = await request(app).get('/api/tasks')
+        // expect(Array.isArray(res.body).toBe(true));
+        expect(typeof res.body).toBe("object");
+        expect(res.body).toHaveProperty("tasks")
+
+    })
+})
+
+afterAll(async()=>{
+    await mongoose.connection.close();
+    await server.close();
+})
